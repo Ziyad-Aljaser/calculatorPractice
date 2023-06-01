d4cd64c4ae307de2f11723c2599b50a6bec3ea56
Ziyad-Aljaser <120469041+Ziyad-Aljaser@users.noreply.github.com>
2023-06-01 13:06:47 -0400

    Primary Implementation Calculator Done

---
+++ practice.js
@@ -0,0 +1,17 @@
+class Bag {
+    color=null;
+    length=0;
+    meterial=null;
+
+    Bag(){
+        this.color=color;
+        this.length=length;
+        this.meterial=meterial
+    }
+
+    Bag(doubleTheLength){
+        this.color=color;
+        this.length=length*doubleTheLength;
+        this.meterial=meterial
+    }
+}
